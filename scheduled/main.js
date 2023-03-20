bff84f92401ac275e3988bb0cbd5a90533a56ee5
Felipe Dias Sardim Lisbao <CIANDT\flisbao@lnb028273cps.ciandt.global>
2023-03-20 02:57:43 -0300

    adding telegram

--- scheduled/main.js
+++ scheduled/main.js
@@ -1,13 +1,19 @@
 const axios = require("axios")
-const {generateDevotional} = require("./generateDevotional")
+const { generateDevotional } = require("./generateDevotional")
 
 async function main() {
-    const text = await generateDevotional();
-    await axios.post(`${process.env.URL}/api/devotional`, { text }, {
-        headers: {
-            'Authorization': `Bearer ${process.env.APP_KEY}`
-        }
-    });
+    try {
+        const text = await generateDevotional();
+        await axios.post(`${process.env.URL}/api/devotional`, { text }, {
+            headers: {
+                'Authorization': `Bearer ${process.env.APP_KEY}`
+            }
+        });
+    } catch (ex) {
+        console.error(`an error happenes ${ex.message}`)
+    }
+
+
 }
 
 main();
--- src/pages/api/devotional.js
+++ src/pages/api/devotional.js
@@ -1,9 +1,11 @@
 const {database} = require("@/config/firebaseConfig");
-const telegram = require("@/lib/telegram_response");
+const TelegramBot = require('node-telegram-bot-api');
 
 export default async function handler(req, res) {
-    const { APP_KEY } = process.env;
+    const { APP_KEY, TELEGRAM_BOT } = process.env;
     const { ACTION_KEY } = req.headers.authorization.split(" ")[1];
+
+    const bot = new TelegramBot(TELEGRAM_BOT, {polling: false});
 
     if (ACTION_KEY !== ACTION_KEY) {
         res.status(401).send('unauthorized');
@@ -15,8 +17,8 @@ export default async function handler(req, res) {
     const subscriptionDocuments =  await db.collection('inscricoes').get();
     console.log('made call to subscriotion')
     subscriptionDocuments.forEach(async doc => {
-        console.log('reading docs')
-        await telegram.sendMessage(text, doc.get('chatId'))
+        console.log('reading docs', JSON.stringify(doc.data()))
+        bot.sendMessage(doc.get('chatId'), text)
     });
     console.log('finished')
     res.json({});
