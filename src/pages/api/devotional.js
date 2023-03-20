c74a9b351652a4868dea1820bf8d94eb49d5056c
Felipe Dias Sardim Lisbao <CIANDT\flisbao@lnb028273cps.ciandt.global>
2023-03-20 09:41:37 -0300

    test

--- src/pages/api/devotional.js
+++ src/pages/api/devotional.js
@@ -1,26 +1,30 @@
-const {database} = require("@/config/firebaseConfig");
+const { database } = require("@/config/firebaseConfig");
 const TelegramBot = require('node-telegram-bot-api');
 
 export default async function handler(req, res) {
     const { APP_KEY, TELEGRAM_BOT } = process.env;
     const { ACTION_KEY } = req.headers.authorization.split(" ")[1];
 
-    const bot = new TelegramBot(TELEGRAM_BOT, {polling: false});
+    const bot = new TelegramBot(TELEGRAM_BOT, { polling: false });
 
-    if (ACTION_KEY !== ACTION_KEY) {
+    if (ACTION_KEY !== APP_KEY) {
         res.status(401).send('unauthorized');
         return;
     }
 
-    const text = req.body.text;
-    const db = database();
-    const subscriptionDocuments =  await db.collection('inscricoes').get();
-    console.log('made call to subscriotion')
-    subscriptionDocuments.forEach(async doc => {
-        console.log('reading docs', JSON.stringify(doc.data()))
-        console.log('text => ', text)
-        bot.sendMessage(doc.get('chatId'), text)
-    });
-    console.log('finished')
-    res.json({});
+    try {
+        const text = req.body.text;
+        const db = database();
+        const subscriptionDocuments = await db.collection('inscricoes').get();
+        console.log('made call to subscriotion')
+        subscriptionDocuments.forEach(async doc => {
+            console.log('reading docs', JSON.stringify(doc.data()))
+            console.log('text => ', text)
+            await bot.sendMessage(doc.get('chatId'), text)
+        });
+        console.log('finished')
+        res.json({});
+    } catch (ex) {
+        console.error(`Error when calling ${ex.message}`)
+    }
 }
