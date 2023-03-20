b4eb921e63bc692a00eb40f105ebbab98a478dcb
Felipe Dias Sardim Lisbao <CIANDT\flisbao@lnb028273cps.ciandt.global>
2023-03-20 17:31:11 -0300

    devotional7

--- src/pages/api/genDevocional.js
+++ src/pages/api/genDevocional.js
@@ -1,25 +1,42 @@
-import {extractMessage} from '@/domain/user';
+import { extractMessage } from '@/domain/user';
 import db from "@/config/firebaseConfig"
 import TelegramBot from 'node-telegram-bot-api';
 
 export default async function handler(req, res) {
 
-    const data = extractMessage(req.body);
-    console.log(JSON.stringify(data));
-    const { TELEGRAM_BOT } = process.env;
-    const bot = new TelegramBot(TELEGRAM_BOT, { polling: false });
+  const data = extractMessage(req.body);
+  console.log(JSON.stringify(data));
+  const { TELEGRAM_BOT } = process.env;
+  const bot = new TelegramBot(TELEGRAM_BOT, { polling: false });
 
-    if (data.message.toLowerCase().startsWith('inscrever')) {
-      
+  if (data.message.toLowerCase().startsWith('inscrever')) {
+
+    const docs = await db.collection("inscricoes").where("chatId", "==", data.chatId).get();
+
+    if (docs.empty) {
       await db.collection("inscricoes").add({
         user: { ...data.user },
         chatId: data.chatId
       })
-
       await bot.sendMessage(data.chatId, "usuário subscrito")
-      res.json({})
-      return;
+    } else {
+      await bot.sendMessage(data.chatId, "usuário já existente")
     }
-    await bot.sendMessage(data.chatId, "comando não encontrado");
+
+    const subscriptionDocuments = await db.collection('inscricoes').get();
+    const currentDevotionalDocuments = await db.collection('devotional').get();
+    let devotional = '';
+    currentDevotionalDocuments.forEach(doc => {
+      devotional = doc.get('devotional')
+    })
+
+    await bot.sendMessage(data.chatId, "Segue abaixo o último devocional")
+    await bot.sendMessage(data.chatId, devotional);
+
+
     res.json({})
+    return;
+  }
+  await bot.sendMessage(data.chatId, "comando não encontrado");
+  res.json({})
 }
